05a10f5e58194d4f7c598e1fea3aa6db1a3fd71d
zahruddin <zahruddin.fanani17@gmail.com>
2024-06-02 20:41:46 +0700

    merapikan saja

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,8 @@ use App\Http\Controllers\Admin\AdminController;
 Route::get('/', [FrontController::class, 'index'])->name('front');
 
 Route::get('/jobs', [JobController::class, 'index'])->name('jobs');
-    Route::get('/jobs/search', [JobController::class, 'search'])->name('jobs.search');
-    Route::get('/job/{id}', [JobController::class, 'jobDetail'])->name('jobDetail');
+Route::get('/jobs/search', [JobController::class, 'search'])->name('jobs.search');
+Route::get('/job/{id}', [JobController::class, 'jobDetail'])->name('jobDetail');
 
 
 Route::get('/login-jobseeker', [LoginController::class, 'showLoginFormJobSeeker'])->name('loginJobseeker');
@@ -43,14 +43,14 @@ Route::middleware('guest')->group(function () {
     Route::post('/admin', [LoginController::class, 'authenticate'])->name('cekloginadmin');
     Route::post('/company', [LoginController::class, 'authenticate'])->name('ceklogincompany');
     Route::post('/jobseeker', [LoginController::class, 'authenticate'])->name('cekloginjobseeker');
-    
+
     Route::get('/company/register', [CompanyController::class, 'showRegistrationForm'])->name('company.register');
     Route::post('/company/register', [CompanyController::class, 'register'])->name('company.register.submit');
 
     Route::get('/jobseeker/register', [JobSeekerController::class, 'showRegistrationForm'])->name('jobseeker.register');
     Route::post('/jobseeker/register', [JobSeekerController::class, 'register'])->name('jobseeker.register.submit');
 
-    
+
     Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 });
 
@@ -64,7 +64,6 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/admin/datacompany/{id}/edit', [KelCompanyController::class, 'showEditCompany'])->name('admin.companyedit');
     Route::post('/admin/datacompany/{id}/edit', [KelCompanyController::class, 'updateCompany'])->name('admin.editcompany');
     Route::delete('/admin/{id}/delete', [KelCompanyController::class, 'deleteCompany'])->name('admin.deletecompany');
-
 });
 
 Route::middleware(['auth', 'company'])->group(function () {
@@ -72,7 +71,7 @@ Route::middleware(['auth', 'company'])->group(function () {
     Route::get('/company/setting', [CompanyController::class, 'showSetting'])->name('company.setting');
     Route::get('/company/profile', [CompanyController::class, 'showProfile'])->name('company.profile');
     Route::post('/company/updateprofile', [CompanyController::class, 'updateProfile'])->name('company.updateprofile');
-    
+
     // Routes for managing jobs
     Route::get('/company/jobs', [JobListingController::class, 'showJobs'])->name('company.jobs');
     Route::get('/company/addjob', [JobListingController::class, 'showAddJob'])->name('company.showaddjob');
@@ -84,14 +83,13 @@ Route::middleware(['auth', 'company'])->group(function () {
     Route::put('/company/{id}/updatestatus', [JobListingController::class, 'updateStatus'])->name('company.editstatus');
     Route::get('/company/jobs/search', [JobListingController::class, 'ajaxSearch'])->name('company.searchjob');
     Route::delete('/company/{id}/delete', [JobListingController::class, 'deleteJob'])->name('company.deletejob');
-    
+
     // Route for managing job applications
     Route::get('/company/lamaranmasuk', [ApplyJobController::class, 'showLamaranMasuk'])->name('company.lamaranmasuk');
     Route::post('/company/lamaranmasuk/{id}/reject', [ApplyJobController::class, 'rejectLamaran'])->name('company.lamaranmasuk.reject');
     Route::post('/company/lamaranmasuk/{id}/accept', [ApplyJobController::class, 'acceptLamaran'])->name('company.lamaranmasuk.accept');
 
     Route::get('/company/job-seeker/{id}', [JobSeekerController::class, 'show'])->name('company.jobseeker.detail');
-
 });
 
 
@@ -109,7 +107,6 @@ Route::middleware(['auth', 'job_seeker'])->group(function () {
 
     //ini tambahan
     Route::post('/job/{id}/apply', [JobSeekerApplyJobController::class, 'applyJob'])->name('applyJob');
-
 });
 
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
